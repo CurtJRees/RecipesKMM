2deceea3b776f544c8dea179c5473015cd1f6381
CurtJRees <curtjrees@gmail.com>
2020-11-15 17:24:10 +0000

    Setup PostgresDb connection with Exposed

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,8 +4,13 @@ plugins {
     kotlin("plugin.serialization")
 }
 
+repositories {
+    maven("https://dl.bintray.com/kotlin/exposed")
+}
+
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlinCoroutines}")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:${Versions.kotlinxSerialization}") // JVM dependency
 
     implementation("io.ktor:ktor-server-core:${Versions.ktor}")
     implementation("io.ktor:ktor-server-netty:${Versions.ktor}")
@@ -13,7 +18,8 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:${Versions.kotlinxSerialization}") // JVM dependency
+    implementation("org.jetbrains.exposed:exposed:0.17.7")
+    implementation("org.postgresql:postgresql:42.2.18")
 
     implementation(project(":sharedCore"))
 }
--- backend/src/main/kotlin/com/curtjrees/recipes/server/Server.kt
+++ backend/src/main/kotlin/com/curtjrees/recipes/server/Server.kt
@@ -1,8 +1,7 @@
-
 package com.curtjrees.recipes.server
 
-import com.curtjrees.recipes.sharedCore.Recipe
 import com.curtjrees.recipes.sharedCore.ApiRecipesResponse
+import com.curtjrees.recipes.sharedCore.Recipe
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.response.*
@@ -10,10 +9,33 @@ import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    val port = System.getenv("PORT")?.toInt() ?: 9090
+    //Database setup
+    Database.connect(System.getenv("JDBC_DATABASE_URL"), driver = "org.postgresql.Driver")
 
+    transaction {
+        addLogger(StdOutSqlLogger)
+        SchemaUtils.create(Recipes)
+
+//        DbRecipe.new {
+//            name = "First DB Recipe"
+//        }
+        println("Recipes: ${DbRecipe.all().joinToString { it.name }}")
+    }
+
+
+    //Server setup
+    val port = System.getenv("PORT")?.toInt() ?: 9090
     embeddedServer(factory = Netty, port = port) {
         install(ContentNegotiation) {
             json()
@@ -22,14 +44,37 @@ fun main() {
         routing {
 
             get("/recipes") {
-                val recipe = Recipe(0L, "First Recipe")
+                val dbRecipes = transaction { DbRecipe.all().toList() }
+                val apiRecipes = dbRecipes.map {
+                    Recipe(it.id.value.toLong(), it.name)
+                }
+
                 val response = ApiRecipesResponse(
                     status = 200,
-                    data = listOf(recipe)
+                    data = apiRecipes
                 )
                 call.respond(response)
+
+
+//                val recipe = Recipe(0L, "First Recipe")
+//                val response = ApiRecipesResponse(
+//                    status = 200,
+//                    data = listOf(recipe)
+//                )
+//                call.respond(response)
             }
 
         }
     }.start(wait = true)
+}
+
+
+object Recipes : IntIdTable() {
+    val name = varchar("name", 255)
+}
+
+class DbRecipe(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<DbRecipe>(Recipes)
+
+    var name by Recipes.name
 }
