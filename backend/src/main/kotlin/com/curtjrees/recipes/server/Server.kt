827eed18135c199990f08b608b19dfe066d578c4
CurtJRees <curtjrees@gmail.com>
2020-12-06 16:36:03 +0000

    Fix server running

--- backend/src/main/kotlin/com/curtjrees/recipes/server/Server.kt
+++ backend/src/main/kotlin/com/curtjrees/recipes/server/Server.kt
@@ -15,8 +15,12 @@ import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
+fun main() {
+    setupDatabase()
+    startServer()
+}
+
 fun Application.main() {
-    //Database setup
     setupDatabase()
     startServer()
 }
