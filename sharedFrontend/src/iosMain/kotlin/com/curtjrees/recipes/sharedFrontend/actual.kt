c1c144c63a31d97c25124eaa49cbf430d6e3af15
CurtJRees <curtjrees@gmail.com>
2020-12-21 17:16:07 +0000

    Fix iOS build issue

--- sharedFrontend/src/iosMain/kotlin/com/curtjrees/recipes/sharedFrontend/actual.kt
+++ sharedFrontend/src/iosMain/kotlin/com/curtjrees/recipes/sharedFrontend/actual.kt
@@ -2,7 +2,6 @@ package com.curtjrees.recipes.sharedFrontend
 
 import com.curtjrees.recipes.sharedFrontend.db.RecipeDatabase
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
-import com.curtjrees.recipes.sharedFrontend.db.RecipeDatabase
 
 actual fun createDb(): RecipeDatabase? {
     val driver = NativeSqliteDriver(RecipeDatabase.Schema, "recipe.db")
